e23d91c1c14ad1bc253e9c6fe690fefe2580f5ab
local name <jamesson_seren@hotmail.com>
2024-08-09 15:14:08 -0400

    create document  validation unity test completed

--- src/modules/AppUser/AppUserManagement/repositories/implementations-document-validation/app-user-document-validation-prisma.repository.ts
+++ src/modules/AppUser/AppUserManagement/repositories/implementations-document-validation/app-user-document-validation-prisma.repository.ts
@@ -1,20 +1,30 @@
+import { Uuid } from "../../../../../@shared/ValueObjects/uuid.vo";
 import { prismaClient } from "../../../../../infra/databases/prisma.config";
 import { newDateF } from "../../../../../utils/date";
 import { DocumentValidationEntity, DocumentValidationProps } from "../../entities/app-user-document-validation.entity";
 import { IAppUserDocumentValidationRepository } from "../app-user-document-validation.repository";
 
 export class DocumentValidationPrismaRepository implements IAppUserDocumentValidationRepository {
-    async find(id: string): Promise<DocumentValidationEntity | null> {
+    create(entity: DocumentValidationEntity): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    update(entity: DocumentValidationEntity): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    findAll(): Promise<DocumentValidationEntity[]> {
+        throw new Error("Method not implemented.");
+    }
+    async find(id: Uuid): Promise<DocumentValidationEntity | null> {
         const document = await prismaClient.userDocumentValidation.findUnique({
             where:{
-                uuid: id
+                uuid: id.uuid
             }
         })
 
         if(!document) return null
 
         return {
-            uuid: document.uuid,
+            uuid: new Uuid(document.uuid),
             document_front_base64: document.document_back_base64,
             document_front_status: document.document_front_status,
             document_back_base64: document.document_back_base64,
@@ -24,18 +34,18 @@ export class DocumentValidationPrismaRepository implements IAppUserDocumentValid
             document_selfie_base64: document.document_selfie_base64,
             document_selfie_status: document.document_selfie_status
 
-        }
+        } as DocumentValidationEntity
     }
     
-    async save(data: DocumentValidationEntity, user_info_uuid: string): Promise<void> {
+    async saveOrUpdate(data: DocumentValidationEntity, user_info_uuid: Uuid): Promise<void> {
 
         await prismaClient.$transaction([
             prismaClient.userDocumentValidation.upsert({
                 where:{
-                    uuid: data.uuid
+                    uuid: data.uuid.uuid
                 },
                 create: {
-                    uuid: data.uuid,
+                    uuid: data.uuid.uuid,
                     document_front_base64: data.document_front_base64,
                     document_front_status: data.document_front_status,
                     document_back_base64: data.document_back_base64,
@@ -44,7 +54,7 @@ export class DocumentValidationPrismaRepository implements IAppUserDocumentValid
                     selfie_status: data.selfie_status,
                     document_selfie_base64: data.document_selfie_base64,
                     document_selfie_status: data.document_selfie_status,
-                    created_at: newDateF(new Date())
+                    created_at: data.created_at
                 },
                 update:{
                     document_front_base64: data.document_front_base64,
@@ -55,18 +65,18 @@ export class DocumentValidationPrismaRepository implements IAppUserDocumentValid
                     selfie_status: data.selfie_status,
                     document_selfie_base64: data.document_selfie_base64,
                     document_selfie_status: data.document_selfie_status,
-                    updated_at: newDateF(new Date())
+                    updated_at:data.updated_at
 
                 }
             }),
 
             prismaClient.userInfo.update({
                 where:{
-                    uuid: user_info_uuid
+                    uuid: user_info_uuid.uuid
                 },
                 data:{
-                    user_document_validation_uuid: data.uuid,
-                    updated_at: newDateF(new Date())
+                    user_document_validation_uuid: data.uuid.uuid,
+                    updated_at: data.updated_at
                 }
             })
         ])
--- src/modules/AppUser/AppUserManagement/usecases/DocumentsValidation/create-documents-validation/create-documents-validation.usecase.ts
+++ src/modules/AppUser/AppUserManagement/usecases/DocumentsValidation/create-documents-validation/create-documents-validation.usecase.ts
@@ -1,45 +1,70 @@
 import { CustomError } from "../../../../../../errors/custom.error";
 import { DocumentValidator } from "../../../../../../utils/document-validation";
 import { DocumentValidationEntity, DocumentValidationProps } from "../../../entities/app-user-document-validation.entity";
+import { IAppUserAuthRepository } from "../../../repositories/app-use-auth-repository";
 import { IAppUserDocumentValidationRepository } from "../../../repositories/app-user-document-validation.repository";
 import { IAppUserInfoRepository } from "../../../repositories/app-user-info.repository";
+import { InputCreateDocumentValidationDTO, OutputCreateDocumentValidationDTO } from "./dto/create-user-validation.dto";
 
 export class CreateDocumentsValidationUsecase {
     constructor(
+        private userAuthRepository: IAppUserAuthRepository,
         private userInfoRepository: IAppUserInfoRepository,
         private documentsValidationRepository: IAppUserDocumentValidationRepository
     ) { }
 
-    async execute(data: DocumentValidationProps, document: string) {
+    async execute(data: InputCreateDocumentValidationDTO): Promise<OutputCreateDocumentValidationDTO> {
 
-        if (!document) throw new CustomError("Document is required", 400)
-        const adjustedDocument = new DocumentValidator()
+        ///find user auth
+        const getUser = await this.userAuthRepository.find(data.user_uuid)
+        if (!getUser) throw new CustomError("User auth not found", 401)
 
-        const validatedDocument = adjustedDocument.validator(document)
-
-        ///find user
-        const getUser = await this.userInfoRepository.findByDocumentUserInfo(validatedDocument)
-        if (!getUser) throw new CustomError("User not found", 400)
+        //find user info
+        const userInfo = await this.userInfoRepository.find(getUser.user_info_uuid)
+        if(!userInfo) throw new CustomError("User info not found", 404)
 
         //Check which document is being sent and update status to 'under_analysis'
         await this.getDocumentType(data)
 
-        if (getUser.user_document_validation_uuid) {
-            const updateDocuments = {
-                uuid: getUser.user_document_validation_uuid.uuid,
-                ...data
+        if (userInfo.user_document_validation_uuid) {
+
+            //find documents validation
+            const documentsValidation = await this.documentsValidationRepository.find(userInfo.user_document_validation_uuid)
+
+            const documentsEntity = new DocumentValidationEntity(documentsValidation)
+
+            documentsEntity.changeDocumentFrontBase64(data.document_front_base64)
+            documentsEntity.changeDocumentBackBase64(data.document_back_base64)
+            documentsEntity.changeDocumentSelfieBase64(data.document_selfie_base64)
+            documentsEntity.changeSelfieBase64(data.selfie_base64)
+            documentsEntity.changeDocumentFrontStatus(data.document_front_status)
+            documentsEntity.changeDocumentBackStatus(data.document_back_status)
+            documentsEntity.changeDocumentSelfieStatus(data.document_selfie_status)
+            documentsEntity.changeSelfieStatus(data.selfie_status)
+            
+            await this.documentsValidationRepository.saveOrUpdate(documentsEntity, userInfo.uuid)
+
+            return {
+                uuid: documentsEntity.uuid,
+                document_front_status: documentsEntity.document_front_status,
+                document_back_status: documentsEntity.document_back_status,
+                document_selfie_status: documentsEntity.document_selfie_status,
+                selfie_status: documentsEntity.selfie_status
             }
-            await this.documentsValidationRepository.save(updateDocuments, getUser.uuid.uuid)
-            return
         }
 
         const documentsEntity = await DocumentValidationEntity.create(data)
-
-        await this.documentsValidationRepository.save(documentsEntity, getUser.uuid.uuid)
-        return
+        await this.documentsValidationRepository.saveOrUpdate(documentsEntity, userInfo.uuid)
+        return {
+            uuid: documentsEntity.uuid,
+            document_front_status: documentsEntity.document_front_status,
+            document_back_status: documentsEntity.document_back_status,
+            document_selfie_status: documentsEntity.document_selfie_status,
+            selfie_status: documentsEntity.selfie_status
+        }
 
     }
-    private async getDocumentType(data: DocumentValidationProps): Promise<void> {
+    private async getDocumentType(data: InputCreateDocumentValidationDTO): Promise<void> {
 
         // Mapear o tipo de documento com base no nome da propriedade
         if (data.document_front_base64) {
