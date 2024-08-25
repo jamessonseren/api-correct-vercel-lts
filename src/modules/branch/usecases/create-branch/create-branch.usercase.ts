f45b0109e9e54ad5a0df56fb36860b75253e9731
Jamesson Arruda <jamesson_seren@hotmail.com>
2024-08-25 10:38:48 -0400

    complete e2e for branchs

--- src/modules/branch/usecases/create-branch/create-branch.usercase.ts
+++ src/modules/branch/usecases/create-branch/create-branch.usercase.ts
@@ -3,17 +3,16 @@ import { BranchEntity } from '../../entities/branch.entity';
 import { CustomError } from '../../../../errors/custom.error';
 
 export class CreateBranchUsecase {
-    constructor(private branchRepository: IBranchRepository) {}
+  constructor(private branchRepository: IBranchRepository) { }
 
-    async execute(data: BranchEntity) {
+  async execute(data: BranchEntity) {
 
-        //check if branch name already exist
+    //check if branch name already exist
 
-        const branchName = await this.branchRepository.findByName(data.name)
-        if(branchName) throw new CustomError("Branch name already registered", 409)
-        
-        const branch = await this.branchRepository.create(data);
+    const branchName = await this.branchRepository.findByName(data.name)
+    if (branchName) throw new CustomError("Branch name already registered", 409)
 
-        return branch;
-    }
+    const branch = await this.branchRepository.create(data);
+    return branch;
+  }
 }
