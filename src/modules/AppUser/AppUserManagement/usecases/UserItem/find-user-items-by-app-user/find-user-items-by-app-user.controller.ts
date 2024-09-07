722b9b58979901d9b981d3876aa1de6069ef8c75
Jamesson Arruda <jamesson_seren@hotmail.com>
2024-09-07 08:40:15 -0400

    unity tests for find all user items by user

---
+++ src/modules/AppUser/AppUserManagement/usecases/UserItem/find-user-items-by-app-user/find-user-items-by-app-user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { IAppUserItemRepository } from "../../../repositories/app-user-item-repository";
+import { IAppUserInfoRepository } from "../../../repositories/app-user-info.repository";
+import { FindAllUserItemsByAppUserUsecase } from "./find-user-items-by-app-user.usecase";
+export class  FindAllUserItemsByEmployeryController {
+  constructor(
+    private appUserItemRepository: IAppUserItemRepository,
+    private appUserInfoRepository: IAppUserInfoRepository
+
+  ) { }
+
+  async handle(req: Request, res: Response) {
+    try {
+      const userInfoUuid = req.appUser.user_info_uuid
+
+      const usecase = new FindAllUserItemsByAppUserUsecase(this.appUserItemRepository, this.appUserInfoRepository)
+
+      const result = await usecase.execute(userInfoUuid)
+
+      return res.json(result)
+    } catch (err: any) {
+      return res.status(err.statusCode).json({
+        error: err.message
+      })
+    }
+  }
+}
