bb5507e24e65d5c105c906e3ff5f4eac7714444e
Jamesson Arruda <jamesson_seren@hotmail.com>
2025-05-24 18:54:07 -0400

    fixed build bugs

--- src/modules/Payments/Accounts/usecases/repositories/business-account.repository.ts
+++ src/modules/Payments/Accounts/usecases/repositories/business-account.repository.ts
@@ -1,5 +1,5 @@
-import RepositoryInterface from "../../../../@shared/domain/repository/repository-interface";
-import { BusinessAccountEntity } from "../entities/business-account.entity";
+import RepositoryInterface from "../../../../../@shared/domain/repository/repository-interface";
+import { BusinessAccountEntity } from "../../entities/business-account.entity";
 
 export interface IBusinessAccountRepository extends RepositoryInterface<BusinessAccountEntity> {
   findByBusinessId(businessId: string): Promise<BusinessAccountEntity | null>;
